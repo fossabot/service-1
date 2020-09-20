072129fc13f562ba341ebc5a02b5cb72fd05e2e1
Andreas Thomas <dev@chronark.com>
2020-09-20 09:15:48 +0200

    Add tests

--- internal/auth/model/group.go
+++ internal/auth/model/group.go
@@ -13,11 +13,23 @@ type Group struct {
 	Name string `json:"name" gorm:"unique"`
 }
 
-func NewGroup(name string) (Group, error) {
+func NewGroup(name string, uuid UUID, time Time) (Group, error) {
 	return Group{
 		ID:        uuid.New(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 		Name:      name,
 	}, nil
+}
+
+// UUID in a separate interface allows us to use inject any uuid implementation.
+// This is already used for testing.
+type UUID interface {
+	New() uuid.UUID
+}
+
+// Time in a separate interface allows us to use inject any time implementation.
+// This is already used for testing.
+type Time interface {
+	Now() time.Time
 }
--- internal/auth/model/group_test.go
+++ internal/auth/model/group_test.go
@@ -1,13 +1,34 @@
 package model
 
 import (
+	"github.com/google/uuid"
 	"reflect"
 	"testing"
+	"time"
 )
 
+type MockTime struct{}
+
+func (mt MockTime) Now() time.Time {
+	testTime, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	return testTime
+}
+
+type MockUUID struct{}
+
+func (mu MockUUID) New() uuid.UUID {
+	testUUID, _ := uuid.FromBytes([]byte("seed"))
+	return testUUID
+}
+
 func TestNewGroup(t *testing.T) {
+	wantID := MockUUID{}.New()
+	wantTime := MockTime{}.Now()
+
 	type args struct {
 		name string
+		uuid UUID
+		time Time
 	}
 	tests := []struct {
 		name    string
@@ -15,11 +36,20 @@ func TestNewGroup(t *testing.T) {
 		want    Group
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "successfully creates a new group",
+			args: args{name: "Example Group", uuid: MockUUID{}, time: MockTime{}},
+			want: Group{
+				ID:        wantID,
+				CreatedAt: wantTime,
+				UpdatedAt: wantTime,
+				Name:      "Example Group",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewGroup(tt.args.name)
+			got, err := NewGroup(tt.args.name, tt.args.uuid, tt.args.time)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewGroup() error = %v, wantErr %v", err, tt.wantErr)
 				return
